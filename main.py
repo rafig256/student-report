70bc05e362837751c0c51485c4821f1b813d2ddc
rafig256 <rafig_256@yahoo.com>
2025-04-15 20:24:07 +0330

    calculate

--- main.py
+++ main.py
@@ -17,4 +17,25 @@ grades_full['rank_in_group'] = grades_full.groupby(['l_id','group'])['score'] \
 grades_full['rank_in_lesson'] = grades_full.groupby('l_id')['score'] \
     .rank(method='dense', ascending=False).astype(int)
 
-print (grades_full)
+grades_full['weighted_score'] = grades_full['score'] * grades_full['factor']
+
+weighted_avg = grades_full.groupby(['st_id', 'level_student']).apply(
+    lambda df: pd.Series({
+        'total_weighted': (df['score'] * df['factor']).sum(),
+        'total_factors': df['factor'].sum()
+    })
+).reset_index()
+
+weighted_avg['weighted_avg'] = weighted_avg['total_weighted'] / weighted_avg['total_factors']
+
+# محاسبه‌ی رتبه در پایه
+weighted_avg['rank_in_level'] = weighted_avg.groupby('level_student')['weighted_avg'] \
+    .rank(method='dense', ascending=False)
+
+grades_full = grades_full.merge(
+    weighted_avg[['st_id', 'rank_in_level', 'weighted_avg']],
+    on='st_id'
+)
+
+# ذخیره فایل اکسل داخل پوشه report
+grades_full.to_excel("report_output/grades_report.xlsx", index=False)
